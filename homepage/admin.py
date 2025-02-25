95de0c28c10e22365f60ddcb1c91f8b943d5e09f
AhmedHamdy85 <ahme.hamdy85@gmail.com>
2025-02-25 15:49:14 +0200

    register models in admin

--- homepage/admin.py
+++ homepage/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import User, Project, Category, Donation
+
 # Register your models here.
+
+
+admin.site.register(User)
+admin.site.register(Project)
+admin.site.register(Category)
+admin.site.register(Donation)
+
+
