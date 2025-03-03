446ff1740f1a2ca3cb85cbd48d3c0c14c2d2004f
YoustinaWilliam <yw1124@fayoum.edu.eg>
2025-03-04 00:12:35 +0200

    project details

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import delete_profile, update_profile, user_profile
+from .views import delete_profile, project_detail, update_profile, user_profile
 
 urlpatterns = [
     path('<int:user_id>/', user_profile, name='user_profile'),
     path('profile/<int:user_id>/update/', update_profile, name='update_profile'),  
     path('profile/<int:user_id>/delete/', delete_profile, name='delete_profile'),
+    path('project/<int:project_id>/', project_detail, name='project_detail'),
+
 ]
