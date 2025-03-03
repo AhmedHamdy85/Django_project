817541ac3265086ba4a0f837056d487f0b30f836
jackiee211 <nggaman111@gmail.com>
2025-03-03 21:19:30 +0200

    solved conflicts

--- create_project/forms.py
+++ create_project/forms.py
@@ -7,6 +7,12 @@ class ProjectForm(ModelForm):
         model = Project
         fields = '__all__'
         widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Project Title'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter Project Description'}),
+            'image': forms.FileInput(attrs={'class': 'form-control'}),
+            'Category': forms.Select(attrs={'class': 'form-control'}),
+            'totalTarget': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Enter Total Target'}),
+            'user': forms.Select(attrs={'class': 'form-control'}),
             'endTime': forms.DateInput(attrs={'type': 'date'}),
         }
 
