390f048bcdbb8b296461af23f0d3ea76130c44be
AhmedHamdy85 <ahme.hamdy85@gmail.com>
2025-03-04 00:09:37 +0200

    solve home erorr and add comment in project  details page

--- homepage/urls.py
+++ homepage/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('project_detail/<int:project_id>/', views.project_detail, name='project_detail'),
+    path('add_comment/<int:project_id>/', views.add_comment, name='add_comment'),
     ]
