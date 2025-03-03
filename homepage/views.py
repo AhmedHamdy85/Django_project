fa79f1bac75a8885a21f8f01c9ecff30712685ac
AhmedHamdy85 <ahme.hamdy85@gmail.com>
2025-03-03 11:15:50 +0200

    create details page and add views and urls

--- homepage/views.py
+++ homepage/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from .models import Project
 
 # Create your views here.
 
 
-def index(request):
-    return render(request, 'homepage/index.html')
+def home(request):
+    return render(request, 'homepage/home.html')
+
+def project_detail(request, project_id):
+    # project=Project.objects.get(id=project_id)
+    return render(request, 'homepage/project_detail.html', {'project_id':project_id})
+
