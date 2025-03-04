18ad0b78e03f46236d470977b9f5ea0b407f8a7a
AhmedHamdy85 <ahme.hamdy85@gmail.com>
2025-03-04 10:48:50 +0200

    finish functinality in project details page

--- homepage/urls.py
+++ homepage/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('project_detail/<int:project_id>/', views.project_detail, name='project_detail'),
     path('add_comment/<int:project_id>/', views.add_comment, name='add_comment'),
+    path('add_donation/<int:project_id>/', views.add_donation, name='add_donation'),
     ]
